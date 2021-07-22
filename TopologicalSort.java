22901d79f3c1a9a1b9ca524d1782b1175e47018b
sharvin18 <dedhiasharvin@gmail.com>
2021-07-22 22:31:20 +0530

    Easy-Medium

---
+++ TopologicalSort.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+// Implement Topological Sorting
+
+
+public class TopologicalSort {
+
+    static ArrayList<Integer> visited;
+    static Stack<Integer> result;
+
+    static void dfs(Graph graph, int currNode){
+
+        if(visited.contains(currNode)) return;
+
+        visited.add(currNode);
+
+        for(int vertex: graph.list.get(currNode)){
+            dfs(graph, currNode);
+        }
+
+        result.push(currNode);
+    }
+
+    static void topoSort(Graph graph, int nodeCount){
+
+        visited = new ArrayList<>();
+        result = new Stack<>();
+
+        for(int i=0; i<nodeCount; i++){
+            if(!visited.contains(i)){
+                dfs(graph, i);
+            }
+        }
+
+        while(!result.isEmpty()){
+            System.out.println(result.pop() + " ");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Edge> edge = new ArrayList<>();
+        System.out.println("Enter number of nodes: ");
+        int nodes = scan.nextInt();
+
+        System.out.println("Enter number of edges: ");
+        int edges = scan.nextInt();
+
+        for(int i=0; i<edges; i++){
+            edge.add(new Edge(scan.nextInt(), scan.nextInt()));
+        }
+
+        Graph graph = new Graph(edge, nodes);
+
+        topoSort(graph, nodes);
+
+        scan.close();
+    }
+}
